572a5e05b658c0f7daaaf992d58d14080f1b9a4c
diegovargasj <diego.vargas.j@ing.uchile.cl>
2018-05-02 13:33:29 -0300

    Se crea modelo de artículo

--- tarea3_isw/models.py
+++ tarea3_isw/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+	name = models.CharField(max_length=255)
+	id = models.CharField(max_length=32)
+	desc = models.CharField(max_length=1024)
+	photo = models.ImageField()
+	state = models.ExpressionList("Disponible", "Prestado", "En reparación", "Perdido")
+
+
+
+
