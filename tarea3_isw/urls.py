1c1b480c1a95f6c227c998b62b68830a821034c2
diegovargasj <diego.vargas.j@ing.uchile.cl>
2018-05-09 15:18:58 -0300

    Url vacio llega a login

--- tarea3_isw/urls.py
+++ tarea3_isw/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='login'),
     path('ficha-articulo/', views.ficha_articulo, name='ficha_articulo'),
     path('landing-page-admin/', views.landing_page_admin, name='landing_page_admin'),
     path('landing-page-pn/', views.landing_page_pn, name='landing_page_pn'),
