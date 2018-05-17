c092dd15508dff7c440a7c9dfc048f3c22d17bb2
CristobalM <cristobal.miranda.tt@gmail.com>
2018-05-17 15:13:26 -0400

    Cambios a la estructura del landing page persona natural, separando articulos y espacios en paginas distintas

--- tarea3_isw/views.py
+++ tarea3_isw/views.py
@@ -1,5 +1,5 @@
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login as django_auth_login, logout as django_auth_logout
 
 from django.contrib.auth.decorators import login_required
@@ -16,11 +16,11 @@ def common_context_logged(request):
 
 	return context
 
+
 @login_required
 def index(request):
 	template = loader.get_template('base.html')
 	context = {
-		'nothing': 0
 	}
 	context = {**context, **common_context_logged(request)}
 	return HttpResponse(template.render(context, request))
@@ -29,7 +29,6 @@ def index(request):
 def ficha_articulo(request):
 	template = loader.get_template('ficha_articulo.html')
 	context = {
-		'nothing': 0
 	}
 	context = {**context, **common_context_logged(request)}
 	return HttpResponse(template.render(context, request))
@@ -38,25 +37,40 @@ def ficha_articulo(request):
 def landing_page_admin(request):
 	template = loader.get_template('landing_page_admin.html')
 	context = {
-		'nothing': 0
 	}
 	context = {**context, **common_context_logged(request)}
 	return HttpResponse(template.render(context, request))
 
 @login_required
 def landing_page_pn(request):
-	template = loader.get_template('landing_page_pn.html')
+	return HttpResponseRedirect('/landing-page-pn/articulos/')
+
+@login_required
+def landing_page_pn_articulos(request):
+	template = loader.get_template('landing_page_pn/articulos.html')
 	context = {
-		'nothing': 0
+		'class_articulos': 'active',
+		'class_espacios': ''
 	}
 	context = {**context, **common_context_logged(request)}
 	return HttpResponse(template.render(context, request))
 
 @login_required
+def landing_page_pn_espacios(request):
+	template = loader.get_template('landing_page_pn/espacios.html')
+	context = {
+		'class_articulos': '',
+		'class_espacios': 'active'
+	}
+	context = {**context, **common_context_logged(request)}
+	return HttpResponse(template.render(context, request))
+
+
+
+@login_required
 def perfil_usuario_dueno(request):
 	template = loader.get_template('perfil_usuario_dueno.html')
 	context = {
-		'nothing': 0
 	}
 	context = {**context, **common_context_logged(request)}
 	return HttpResponse(template.render(context, request))
@@ -75,12 +89,10 @@ def login(request):
 			else:
 				print("mal user")
 
-
 	template = loader.get_template('login.html')
 	context = {
 		'form': LoginForm()
 	}
-
 
 	return HttpResponse(template.render(context, request))
 
