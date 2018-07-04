bd7acf010cffca0f37e7c5c3ffb1f4b8c4420376
Nicolas <nescudero57@gmail.com>
2018-07-04 16:10:39 -0400

    Merge branch 'master' of https://github.com/DCC-CC4401/2018-1-0xA-T3
    # Conflicts:
    #	tarea3_isw/models.py

--- static/js/main.js
+++ static/js/main.js
@@ -24,3 +24,38 @@ $("#schedule-demo").jqs({
     }
         ]
     });
+
+$("#schedule-espacio-demo").jqs({
+        mode:"read",
+        data: [{
+            day: 0,
+            periods: [{
+                start: "00:00",
+                end: "02:00",
+                title: "Una reserva"
+            },
+            {
+                start: "20:00",
+                end: "00:00",
+                title: "Otra reserva",
+                backgroundColor: "rgba(207, 0, 15, 0.5)"
+            },
+            {
+                start: "19:00",
+                end: "20:00",
+                title: "Otra2 reserva",
+                backgroundColor: "rgba(207, 0, 15, 0.5)"
+            }]
+        }, {
+                day: 2,
+                periods: [
+            {
+                start: "10:00",
+                end: "12:00",
+                title: "Una tercera reserva"
+            }
+        ]
+    }
+        ]
+});
+
