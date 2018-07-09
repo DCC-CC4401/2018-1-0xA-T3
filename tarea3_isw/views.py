242471d61c07f5c30421890076c2b9d4c615e2b3
Nicolas <nescudero57@gmail.com>
2018-07-09 18:33:23 -0400

    Merge branch 'master' of https://github.com/DCC-CC4401/2018-1-0xA-T3 into Nico_Branch
    # Conflicts:
    #	static/js/main.js
    #	tarea3_isw/templates/base.html
    #	tarea3_isw/templates/perfil_usuario_dueno.html

--- tarea3_isw/views.py
+++ tarea3_isw/views.py
@@ -227,6 +227,10 @@ def perfil_usuario_dueno(request):
 	place_history = PlaceReservation.objects.filter(user=request.user) \
 		                .order_by('-init_date')[:10]
 
+	if request.method == 'POST':
+		todel = request.POST.getlist('todelete')
+		ArticleLoan.objects.filter(user=request.user, id__in=todel).delete()
+
 	context = {
 		'article_history': article_history,
 		'place_history': place_history
